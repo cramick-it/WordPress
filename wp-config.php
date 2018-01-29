aa85ab257936e53fbdf8ed23a470b40eef679d8b
ashwinikumar251 <ashwinikumar251@gmail.com>
2018-01-29 11:01:38 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'wordpressuser');
 define('DB_PASSWORD', 'root12345');
 
 /** MySQL hostname */
-define('DB_HOST', 'wp.cvhvlhx1wbfs.eu-west-1.rds.amazonaws.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
