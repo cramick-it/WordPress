14cb8e47d58f011b980fa5419002f3bbec989bc4
ashwinikumar251 <ashwinikumar251@gmail.com>
2018-01-29 10:13:32 +0530

    Update installlamp.sh

--- scripts/installlamp.sh
+++ scripts/installlamp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-yum update -y 
 yum -y install httpd24 > /var/log/installapache.out 2>&1
-yum -y install php70 
+yum -y install php70
 yum -y install php70-mysqlnd
-yum -y install mysql56-server
